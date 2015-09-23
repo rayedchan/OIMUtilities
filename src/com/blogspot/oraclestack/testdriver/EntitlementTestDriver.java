30d1ad0da7e7ac252bc7f2558035accc62c5a351
rayedchan <rayedchan@hotmail.com>
2015-09-22 23:02:18 -0400

    Testing methods to fetch entitlements with criteria

--- src/com/blogspot/oraclestack/testdriver/EntitlementTestDriver.java
+++ src/com/blogspot/oraclestack/testdriver/EntitlementTestDriver.java
@@ -4,10 +4,18 @@ import com.blogspot.oraclestack.utilities.EntitlementUtilities;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
+import oracle.core.ojdl.logging.ODLLevel;
+import oracle.core.ojdl.logging.ODLLogger;
 import oracle.iam.identity.usermgmt.api.UserManager;
 import oracle.iam.platform.OIMClient;
+import oracle.iam.platform.entitymgr.vo.SearchCriteria;
+import oracle.iam.provisioning.api.ApplicationInstanceService;
 import oracle.iam.provisioning.api.EntitlementService;
+import oracle.iam.provisioning.api.ProvisioningConstants;
 import oracle.iam.provisioning.api.ProvisioningService;
+import oracle.iam.provisioning.vo.ApplicationInstance;
+import oracle.iam.provisioning.vo.Entitlement;
 
 /**
  * Test Driver for using entitlement API.
@@ -15,6 +23,8 @@ import oracle.iam.provisioning.api.ProvisioningService;
  */
 public class EntitlementTestDriver 
 {
+    private static final ODLLogger logger = ODLLogger.getODLLogger(EntitlementTestDriver.class.getName());
+    
      // Adjust constant variables according to you OIM environment
     public static final String OIM_HOSTNAME = "localhost";
     public static final String OIM_PORT = "14000"; // For SSL, use 14001; For non-SSL, use 14000
@@ -50,12 +60,13 @@ public class EntitlementTestDriver
             UserManager usrMgr = oimClient.getService(UserManager.class);
             ProvisioningService provServOps = oimClient.getService(ProvisioningService.class);
             EntitlementService entServ = oimClient.getService(EntitlementService.class);
-            
+            ApplicationInstanceService appInstServ = oimClient.getService(ApplicationInstanceService.class);
+                    
             // Instantiate custom entitlement utils
             EntitlementUtilities entUtils = new EntitlementUtilities(provServOps, usrMgr, entServ);
             
             // Print all entitlement definitions
-            entUtils.printEntitlementDefinition();
+            // entUtils.printEntitlementDefinition();
             
             String displayName = "GridGuard";
             String entCode = "21~GridGuard";
@@ -68,14 +79,55 @@ public class EntitlementTestDriver
             // Create Entitlement
             //entUtils.createEntitlement(displayName, entCode, entValue, itResKey, objKey, formKey, formFieldKey, lookupKey);
             
-            String userLogin = "RSYNGAL";
-            String appInstName = "Laptop";
-            String entitlementCode = "Lenovo";
-            String entitlementDisplayName = "Lenovo"; 
-            HashMap<String, Object> entitlementAttributes = new HashMap<String,Object>();
-            entitlementAttributes.put("UD_LPTYPE_STARTDATE", new Date());
-            entitlementAttributes.put("UD_LPTYPE_HARDDRIVESPACE", "300GB");
+            String userId = "3007";
+            String resourceObjectName = "LDAP User";
+            
+            // Get application instance by resource object name
+            SearchCriteria objectNameCriteria =  new SearchCriteria(ProvisioningConstants.AccountSearchAttribute.OBJ_NAME.getId(), resourceObjectName, SearchCriteria.Operator.EQUAL);
+            HashMap<String,Object> configParams = new HashMap<String,Object>();
+            List<ApplicationInstance> appInsts = appInstServ.findApplicationInstance(objectNameCriteria, configParams);
+            ApplicationInstance applicationInstance = null;
+            
+            for(ApplicationInstance appInst : appInsts)
+            {
+                applicationInstance = appInst;
+            }
+            
+            //throw ApplicationInstanceNotFoundException
+            logger.log(ODLLevel.NOTIFICATION, "Found Application Instance: {0}", new Object[]{applicationInstance});
+            
+            // Get Entitlement Defintions
+            Long resourceObjKey = applicationInstance.getObjectKey();
+            SearchCriteria objKeyCriteria = new SearchCriteria(ProvisioningConstants.EntitlementSearchAttribute.OBJ_KEY.getId(),resourceObjKey, SearchCriteria.Operator.EQUAL);     
+            SearchCriteria entCriteria = new SearchCriteria(ProvisioningConstants.EntitlementSearchAttribute.ENTITLEMENT_CODE.getId(), "42~cn=Engineer,ou=Groups,dc=my-domain,dc=com", SearchCriteria.Operator.EQUAL);     
+            SearchCriteria entCriteria2 = new SearchCriteria(ProvisioningConstants.EntitlementSearchAttribute.ENTITLEMENT_CODE.getId(), "42~cn=Scientist,ou=Groups,dc=my-domain,dc=com", SearchCriteria.Operator.EQUAL);
+            SearchCriteria entCriteria3 = new SearchCriteria(ProvisioningConstants.EntitlementSearchAttribute.ENTITLEMENT_VALUE.getId(), "DSEE Server~Administrator", SearchCriteria.Operator.EQUAL);
+            SearchCriteria orEntCriteria = new SearchCriteria(entCriteria, entCriteria2, SearchCriteria.Operator.OR);
+            SearchCriteria orEntCriteria2 = new SearchCriteria(orEntCriteria, entCriteria3, SearchCriteria.Operator.OR);
+            SearchCriteria allCriteria = new SearchCriteria(objKeyCriteria, orEntCriteria2, SearchCriteria.Operator.AND);     
+            
+            List<Entitlement> entitlements = entServ.findEntitlements(allCriteria, configParams);
+            logger.log(ODLLevel.NOTIFICATION, "Entitlement Definition List: {0}", new Object[]{entitlements});
+            
+            // Check if user has entitlement
+            for(Entitlement ent : entitlements)
+            {
+                boolean isEntProvisioned = provServOps.isEntitlementProvisionedToUser(userId, ent);
+                logger.log(ODLLevel.NOTIFICATION, "Is Entitlement [{0}] Provisioned? {1}", new Object[]{ent, isEntProvisioned});
+            }
+            
+            // Disconnected Resource
+            //String userLogin = "RSYNGAL";
+            //String appInstName = "Laptop";
+            //String entitlementCode = "Lenovo";
+            //String entitlementDisplayName = "Lenovo"; 
+            //HashMap<String, Object> entitlementAttributes = new HashMap<String,Object>();
+            //entitlementAttributes.put("UD_LPTYPE_STARTDATE", new Date());
+            //entitlementAttributes.put("UD_LPTYPE_HARDDRIVESPACE", "300GB");
                         
+            // Connected Resource
+            
+            
             // Print user's entitlements
             //entUtils.printUserEntitlementInstances(userLogin);
                               
