c1738d62515899d0d16ff808487b31849f286b7e
rayedchan <rayedchan@hotmail.com>
2016-06-14 22:39:15 -0400

    Ready to be tested

--- src/com/blogspot/oraclestack/scheduledtasks/ReconEventsGeneratorDatabaseSource.java
+++ src/com/blogspot/oraclestack/scheduledtasks/ReconEventsGeneratorDatabaseSource.java
@@ -1,20 +1,15 @@
 package com.blogspot.oraclestack.scheduledtasks;
 
-import Thor.API.Exceptions.tcAPIException;
-import Thor.API.Exceptions.tcColumnNotFoundException;
-import Thor.API.Exceptions.tcInvalidLookupException;
-import Thor.API.Operations.tcLookupOperationsIntf;
-import Thor.API.tcResultSet;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -31,8 +26,12 @@ import oracle.iam.scheduler.vo.TaskSupport;
 
 /**
  * A scheduled task to create reconciliation events of a specified resource object
- * using data from a database table.
+ * using data from a database table. The database table must be constructed in a way where
+ * the column names match the names of the corresponding reconciliation field. Either 
+ * trusted or target resource object can be used as long as the required fields are provided. 
  * @author rayedchan
+ * 
+ * TODO: Child Data 
  */
 public class ReconEventsGeneratorDatabaseSource extends TaskSupport
 {
@@ -41,66 +40,37 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
     
     // OIM API Services
     private ReconOperationsService reconOps = Platform.getService(ReconOperationsService.class);
+    
+    // Default Date Format
+    private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
    
     @Override
-    public void execute(HashMap params) throws NamingException, SQLException, tcAPIException, tcInvalidLookupException, tcColumnNotFoundException
+    public void execute(HashMap params) throws NamingException, SQLException
     {
         LOGGER.log(ODLLevel.NOTIFICATION, "Scheduled Job Parameters: {0}", new Object[]{params});
         Connection conn = null;
-        tcLookupOperationsIntf lookupOps = Platform.getService(tcLookupOperationsIntf.class);
-         
+                
         try
         {
             // Get the parameters from the scheduled job
-            String dataSource = (String) params.get("Data Source"); // JNDI Name for Data Source
+            String dataSource = (String) params.get("Data Source"); // JNDI Name for the Data Source
             String resourceObjectName = (String) params.get("Resource Object Name"); // Reconciliation Profile Name
-            String tableName = (String) params.get("Table Name");
-            String filter = (String) params.get("Filter") == null ? "" : (String) params.get("Filter"); // WHERE clause
-            String reconAttrMapLookup = (String) params.get("Recon Attribute Lookup");
-            String keyAttrName = (String) params.get("Key Attribute Name"); // __UID__
-            // TODO: Boolean if column names are identical to recon field names
-            
-            // Store all reconciliation events to be created
-            List<InputData> allReconEvents = new ArrayList<InputData>();
-            
+            String tableName = (String) params.get("Table Name"); // Database table name
+            String filter = (String) params.get("Filter") == null ? "" : (String) params.get("Filter"); // WHERE clause filter
+            String dateFormat = (String) params.get("Date Format") == null ? DEFAULT_DATE_FORMAT : (String) params.get("Date Format"); // Date Format for reconciliation event E.g. "yyyy-MM-dd"
+            Boolean ignoreDuplicateEvent = (Boolean) params.get("Ignore Duplicate Event"); // Identical to using IgnoreEvent API; if true, reconciliation event won't be created if there is nothing to update
+                        
             // Reconciliation events details
             Boolean eventFinished = true; // No child data provided; mark event to Data Received
-            String dateFormat = "yyyy-MM-dd";
-            Date actionDate = null; // Event to be processed immediately
-            boolean ignoreDuplicate = true; // Identical to using IgnoreEvent API
-            BatchAttributes batchAttrs = new BatchAttributes(resourceObjectName, dateFormat, ignoreDuplicate);
+            Date actionDate = null; // Event to be processed immediately for null. If a date is specified, defer reconciliation event.
+            BatchAttributes batchAttrs = new BatchAttributes(resourceObjectName, dateFormat, ignoreDuplicateEvent);
             
             // Get database connection from data source
             conn = getDatabaseConnection(dataSource);
+            LOGGER.log(ODLLevel.NOTIFICATION, "Retrieved connection for datasource: {0}" , new Object[]{dataSource});
             
-            // Get Recon Attribute Mappings
-            HashMap<String,String> reconAttrMap = convertLookupToMap(lookupOps, reconAttrMapLookup); 
-            
-            // SELECT SQL Query: Users Table
-            String usersQuery = "SELECT * FROM " + tableName + " " + filter;
-            PreparedStatement ps = conn.prepareStatement(usersQuery);
-            ResultSet rs = ps.executeQuery();
-            
-            // Iterate each record
-            while(rs.next())
-            {
-                // Store recon event data 
-                HashMap<String, Serializable> reconEventData = new HashMap<String, Serializable>();
-                
-                // Iterate recon attribute lookup and populate map accordingly
-                for(Map.Entry<String, String> entry : reconAttrMap.entrySet())
-                {
-                    String reconFieldName = entry.getKey(); // Code Key
-                    String targetColumn = entry.getValue(); // Decode
-                    reconEventData.put(reconFieldName, rs.getString(targetColumn));
-                }
-                
-                InputData event = new InputData(reconEventData, null, eventFinished, ChangeType.CHANGELOG, actionDate);
-                
-                // Add recon event to list
-                allReconEvents.add(event);
-            }
-            
+            // Construct list of reconciliation event to be created reading from source database table
+            List<InputData> allReconEvents = constructReconciliationEventList(conn, tableName, filter, eventFinished, actionDate);
             LOGGER.log(ODLLevel.NOTIFICATION, "Recon Events {0}: {1}", new Object[]{allReconEvents.size(), allReconEvents});
             InputData[] events = new InputData[allReconEvents.size()];
             allReconEvents.toArray(events);
@@ -123,34 +93,11 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
             throw e;
         }
         
-        catch (tcAPIException e)
-        {
-            LOGGER.log(ODLLevel.SEVERE, "Could not get Recon Attr Lookup: ", e);
-            throw e;
-        } 
-        
-        catch (tcInvalidLookupException e)
-        {
-            LOGGER.log(ODLLevel.SEVERE, "Could not get Recon Attr Lookup: ", e);
-            throw e;
-        } 
-        
-        catch (tcColumnNotFoundException e) 
-        {
-            LOGGER.log(ODLLevel.SEVERE, "Could not get Recon Attr Lookup: ", e);
-            throw e;
-        }
-
         finally
         {
             if(conn != null)
             {
                 conn.close();
-            }
-            
-            if(lookupOps != null)
-            {
-                lookupOps.close();
             }
         }
     }
@@ -184,31 +131,50 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
     }
     
     /**
-     * Converts a lookup definition into a Map. The Code Key column is used as
-     * the key and the Decode column is used as the value.
-     * @param lookupDefinitionName      Name of the lookup definition
-     * @return Map of lookup values {Key = Code Key, Value = Decode}.
-     * @throws tcAPIException
-     * @throws tcInvalidLookupException
-     * @throws tcColumnNotFoundException
+     * Construct a list of reconciliation events staging to be created
+     * @param conn  Database connection
+     * @param tableName Source table name
+     * @param filter    WHERE clause to be appended to SQL query
+     * @param eventFinished Determine if child data needs to be added
+     * @param actionDate For deferring events
+     * @return List of events to be created
+     * @throws SQLException 
      */
-    public HashMap<String, String> convertLookupToMap(tcLookupOperationsIntf lookupOps, String lookupDefinitionName) throws tcAPIException, tcInvalidLookupException, tcColumnNotFoundException 
+    public List<InputData> constructReconciliationEventList(Connection conn, String tableName, String filter, Boolean eventFinished, Date actionDate) throws SQLException
     {
-        HashMap<String, String> lookupValues = new HashMap<String, String>();
-        tcResultSet lookupValuesRs = lookupOps.getLookupValues(lookupDefinitionName); // Get lookup values
-        int numRows = lookupValuesRs.getTotalRowCount();
+        List<InputData> allReconEvents = new ArrayList<InputData>();
+        
+        // SELECT SQL Query on source table
+        String usersQuery = "SELECT * FROM " + tableName + (filter == null || "".equals(filter) ? "" : " " + filter);
+        PreparedStatement ps = conn.prepareStatement(usersQuery);
+        ResultSet rs = ps.executeQuery();
 
-        // Iterate lookup resultset and construct map
-        for (int i = 0; i < numRows; i++) 
+        // Get the result set metadata
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        LOGGER.log(ODLLevel.NOTIFICATION, "Column count: {0}", new Object[]{columnCount});
+
+        // Iterate each record
+        while(rs.next())
         {
-            lookupValuesRs.goToRow(i);
-            String codeKey = lookupValuesRs.getStringValue("Lookup Definition.Lookup Code Information.Code Key"); // Fetch Code Key
-            String decode = lookupValuesRs.getStringValue("Lookup Definition.Lookup Code Information.Decode"); // Fetch Decode
-            lookupValues.put(codeKey, decode);
-        }
+            // Store recon event data 
+            HashMap<String, Serializable> reconEventData = new HashMap<String, Serializable>();
 
-        return lookupValues;
+            // Iterate recon attribute lookup and populate map accordingly
+            for(int i = 1; i <= columnCount; i++)
+            {
+                String reconFieldName = rsmd.getColumnName(i); // Get column name
+                String value = rs.getString(reconFieldName); // Get column value
+                reconEventData.put(reconFieldName, value);
+            }
+
+            LOGGER.log(ODLLevel.NOTIFICATION, "Recon Event Data: {0}", new Object[]{reconEventData});
+            InputData event = new InputData(reconEventData, null, eventFinished, ChangeType.CHANGELOG, actionDate);
+
+            // Add recon event to list
+            allReconEvents.add(event);
+        }
+        
+        return allReconEvents;
     }
 }
-
-
--- src/com/blogspot/oraclestack/testdriver/GenericReconEventFlatFileFeedTestDriver.java
+++ src/com/blogspot/oraclestack/testdriver/GenericReconEventFlatFileFeedTestDriver.java
@@ -2,8 +2,15 @@ package com.blogspot.oraclestack.testdriver;
 
 import com.blogspot.oraclestack.objects.UserProcessor;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,10 +41,9 @@ import oracle.iam.reconciliation.api.ReconOperationsService;
 import oracle.iam.reconciliation.api.ReconciliationResult;
 import oracle.iam.reconciliation.vo.EventConstants;
 
-
 /**
  * Generic Resource Reconciliation Event Creator
- * Source is from a flat file
+ * Source is from a flat file or database table
  * Any resource can be used as long as the resource object and fields are correctly specified.
  * @author rayedchan
  */
@@ -64,8 +70,8 @@ public class GenericReconEventFlatFileFeedTestDriver
     public static void main(String[] args) throws Exception
     {
         OIMClient oimClient = null;
-        BufferedReader bReader = null;
-        
+         Connection conn = null;
+          
         try
         {
             // Set system properties required for OIMClient
@@ -88,63 +94,32 @@ public class GenericReconEventFlatFileFeedTestDriver
             ReconOperationsService reconOps = oimClient.getService(ReconOperationsService.class);
             EventMgmtService eventService = oimClient.getService(EventMgmtService.class);
             
-            // Input Parameters 
-            String csvFilePath = "/home/oracle/Desktop/psft_hrms_users.csv";
-            String delimiter = ",";
-            String profileName = "Peoplesoft HRMS"; // Reconciliation Profile Name - Resource Object Name
+            String profileName = "";
+            String dataSource = "";
+            String tableName = "";
+            String filter = "";
             
-            // Read File
-            FileReader fReader = new FileReader(csvFilePath); 
-            bReader = new BufferedReader(fReader);
-            
+            // Recon Event Details
             Boolean eventFinished = true; // No child data provided; mark event to Data Received
             String dateFormat = "yyyy-MM-dd";
             Date actionDate = null; // Event to be processed immediately
             boolean ignoreDuplicate = false; // Identical to using IgnoreEvent API
             BatchAttributes batchAttrs = new BatchAttributes(profileName, dateFormat, ignoreDuplicate);
-
-            // Store all data from flat file into OIM object
-            List<InputData> allReconEvents = new ArrayList<InputData>();
             
-            // Header Line
-            String line = bReader.readLine();
-            if(line == null || "".equalsIgnoreCase(line))
-            {
-                throw new Exception("Header must be provided as the first entry in file.");
-            }
-            String[] header = line.split(delimiter);
-            System.out.println(Arrays.asList(header));
+            // Get database connection from data source
+            conn = null;
+            LOGGER.log(ODLLevel.NOTIFICATION, "Retrieved connection for datasource: {0}" , new Object[]{dataSource});
             
-            // Convert each line entry into a map object
-            line = bReader.readLine();
-            while(line != null)
-            {
-                HashMap<String, Serializable> reconEventData = new HashMap<String, Serializable>();
-                String[] entryValues = line.split(delimiter);
-                
-                // One to one correspondence to header
-                for(int i = 0; i < entryValues.length; i++)
-                {
-                    reconEventData.put(header[i],entryValues[i]);
-                }
-               
-                InputData event = new InputData(reconEventData, null, eventFinished, ChangeType.CHANGELOG, actionDate);
-                
-                allReconEvents.add(event);
-                
-                // read next line
-                line = bReader.readLine();
-            }
-            
-            
-            //System.out.println(allReconEvents);
+            // Construct list of reconciliation event to be created reading from source database table
+            List<InputData> allReconEvents = constructReconciliationEventList(conn, tableName, filter, eventFinished, actionDate);
+            LOGGER.log(ODLLevel.NOTIFICATION, "Recon Events {0}: {1}", new Object[]{allReconEvents.size(), allReconEvents});
             InputData[] events = new InputData[allReconEvents.size()];
             allReconEvents.toArray(events);
             
-            //System.out.println(Arrays.asList(events));
+            // Create reconciliation events in OIM and process them
             ReconciliationResult result = reconOps.createReconciliationEvents(batchAttrs, events);
-            System.out.println(result.getSuccessResult());
-            System.out.println(result.getFailedResult());
+            LOGGER.log(ODLLevel.NOTIFICATION, "Success result: {0}",  new Object[]{result.getSuccessResult()});
+            LOGGER.log(ODLLevel.NOTIFICATION, "Success result: {0}",  new Object[]{result.getFailedResult()});
             
             
             
@@ -188,11 +163,134 @@ public class GenericReconEventFlatFileFeedTestDriver
             {
                 oimClient.logout();
             }
-            
+        } 
+    }  
+    
+     /**
+     * Construct a list of reconciliation events staging to be created
+     * @param conn  Database connection
+     * @param tableName Source table name
+     * @param filter    WHERE clause to be appended to SQL query
+     * @param eventFinished Determine if child data needs to be added
+     * @param actionDate For deferring events
+     * @return List of events to be created
+     * @throws SQLException 
+     */
+    public static List<InputData> constructReconciliationEventList(Connection conn, String tableName, String filter, Boolean eventFinished, Date actionDate) throws SQLException
+    {
+        List<InputData> allReconEvents = new ArrayList<InputData>();
+        
+        // SELECT SQL Query on source table
+        String usersQuery = "SELECT * FROM " + tableName + (filter == null || "".equals(filter) ? "" : " " + filter);
+        PreparedStatement ps = conn.prepareStatement(usersQuery);
+        ResultSet rs = ps.executeQuery();
+
+        // Get the result set metadata
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        LOGGER.log(ODLLevel.NOTIFICATION, "Column count: {0}", new Object[]{columnCount});
+
+        // Iterate each record
+        while(rs.next())
+        {
+            // Store recon event data 
+            HashMap<String, Serializable> reconEventData = new HashMap<String, Serializable>();
+
+            // Iterate recon attribute lookup and populate map accordingly
+            for(int i = 1; i <= columnCount; i++)
+            {
+                String reconFieldName = rsmd.getColumnName(i); // Get column name
+                String value = rs.getString(reconFieldName); // Get column value
+                reconEventData.put(reconFieldName, value);
+            }
+
+            LOGGER.log(ODLLevel.NOTIFICATION, "Recon Event Data: {0}", new Object[]{reconEventData});
+            InputData event = new InputData(reconEventData, null, eventFinished, ChangeType.CHANGELOG, actionDate);
+
+            // Add recon event to list
+            allReconEvents.add(event);
+        }
+        
+        return allReconEvents;
+    }
+    
+    /**
+     * Create list of reconciliation events using data from a flat file
+     * @param reconOps  OIM Reconciliation Service
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws Exception 
+     */
+    public static void flatFileFeed(ReconOperationsService reconOps) throws FileNotFoundException, IOException, Exception
+    {
+        BufferedReader bReader = null;
+                
+        // Input Parameters 
+        String csvFilePath = "/home/oracle/Desktop/psft_hrms_users.csv";
+        String delimiter = ",";
+        String profileName = "Peoplesoft HRMS"; // Reconciliation Profile Name - Resource Object Name
+
+        try
+        {
+            // Read File
+            FileReader fReader = new FileReader(csvFilePath); 
+            bReader = new BufferedReader(fReader);
+
+            Boolean eventFinished = true; // No child data provided; mark event to Data Received
+            String dateFormat = "yyyy-MM-dd";
+            Date actionDate = null; // Event to be processed immediately
+            boolean ignoreDuplicate = false; // Identical to using IgnoreEvent API
+            BatchAttributes batchAttrs = new BatchAttributes(profileName, dateFormat, ignoreDuplicate);
+
+            // Store all data from flat file into OIM object
+            List<InputData> allReconEvents = new ArrayList<InputData>();
+
+            // Header Line
+            String line = bReader.readLine();
+            if(line == null || "".equalsIgnoreCase(line))
+            {
+                throw new Exception("Header must be provided as the first entry in file.");
+            }
+            String[] header = line.split(delimiter);
+            System.out.println(Arrays.asList(header));
+
+            // Convert each line entry into a map object
+            line = bReader.readLine();
+            while(line != null)
+            {
+                HashMap<String, Serializable> reconEventData = new HashMap<String, Serializable>();
+                String[] entryValues = line.split(delimiter);
+
+                // One to one correspondence to header
+                for(int i = 0; i < entryValues.length; i++)
+                {
+                    reconEventData.put(header[i],entryValues[i]);
+                }
+
+                InputData event = new InputData(reconEventData, null, eventFinished, ChangeType.CHANGELOG, actionDate);
+
+                allReconEvents.add(event);
+
+                // read next line
+                line = bReader.readLine();
+            }
+
+            //System.out.println(allReconEvents);
+            InputData[] events = new InputData[allReconEvents.size()];
+            allReconEvents.toArray(events);
+
+            //System.out.println(Arrays.asList(events));
+            ReconciliationResult result = reconOps.createReconciliationEvents(batchAttrs, events);
+            System.out.println(result.getSuccessResult());
+            System.out.println(result.getFailedResult());
+        }
+        
+        finally
+        {
             if(bReader != null)
             {
                 bReader.close();
             }
         } 
-    }  
+    }
 }
