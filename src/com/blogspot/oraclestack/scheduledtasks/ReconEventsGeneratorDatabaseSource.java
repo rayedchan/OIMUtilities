7045149092ba2738ec6f954dc92ad36922346df1
rayedchan <rayedchan@hotmail.com>
2016-07-04 22:34:46 -0400

    Continuing on child table feature

--- src/com/blogspot/oraclestack/scheduledtasks/ReconEventsGeneratorDatabaseSource.java
+++ src/com/blogspot/oraclestack/scheduledtasks/ReconEventsGeneratorDatabaseSource.java
@@ -14,8 +14,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.Context;
@@ -41,8 +43,9 @@ import oracle.iam.scheduler.vo.TaskSupport;
  * 
  * Additional features:
  * - Attribute Mapping Translation
- * TODO: Child Data 
+ * - Child Data 
  * TODO: Put batching
+ * TODO: Action Date
  */
 public class ReconEventsGeneratorDatabaseSource extends TaskSupport
 {
@@ -74,6 +77,9 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
             String attrMappings = (String) params.get("Mapping Lookup"); // Correlates target field to recon field 
             String itResName = (String) params.get("IT Resource Name"); // IT Resource Name required for target sources. Empty for trusted sources.
             
+            // Parameters for Child Data
+            String linkColumnName = (String) params.get("Link Column Name"); // Field to relate parent and child table
+            
             // Reconciliation events details
             Boolean eventFinished = true; // No child data provided; mark event to Data Received
             Date actionDate = null; // Event to be processed immediately for null. If a date is specified, defer reconciliation event.
@@ -88,8 +94,13 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
             HashMap<String,String> reconAttrMap = convertLookupToMap(lookupOps, attrMappings);
             LOGGER.log(ODLLevel.NOTIFICATION, "Lookup {0} : {1}" , new Object[]{attrMappings, reconAttrMap});
             
+            // Derive child table mappings
+            HashMap<String,String> childTableMappings = new HashMap<String,String>(); // Key = Multivalued Field Name, Code = Target Child Table Name
+            HashMap<String,HashMap<String,String>> childColumnMappings = new HashMap<String,HashMap<String,String>>(); // Key = Multivalued Field Name, {Key = Field Name, Value = Target Column Name}
+            deriveChildTableMappings(reconAttrMap, childTableMappings, childColumnMappings);
+            
             // Construct list of reconciliation event to be created reading from source database table
-            List<InputData> allReconEvents = constructReconciliationEventList(conn, tableName, filter, eventFinished, actionDate, reconAttrMap, itResName);
+            List<InputData> allReconEvents = constructReconciliationEventList(conn, tableName, filter, eventFinished, actionDate, reconAttrMap, itResName, childTableMappings, childColumnMappings, linkColumnName);
             LOGGER.log(ODLLevel.NOTIFICATION, "Recon Events {0}: {1}", new Object[]{allReconEvents.size(), allReconEvents});
             InputData[] events = new InputData[allReconEvents.size()];
             allReconEvents.toArray(events);
@@ -157,6 +168,60 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
     }
     
     /**
+     * Derive Child Table Mappings
+     * @param reconAttrMap  Reconciliation Filed Mappings; Remove child info once processed
+     * @param childTableMappings Populate reconciliation map name with corresponding child table name
+     * @param childColumnMappings Populate child reconciliation field name with corresponding child column name 
+     */
+    private void deriveChildTableMappings(HashMap<String,String> reconAttrMap, HashMap<String,String> childTableMappings, HashMap<String,HashMap<String,String>> childColumnMappings)
+    {
+        // Iterator for Recon Attr Lookup
+        Iterator<Entry<String,String>> it  = reconAttrMap.entrySet().iterator();
+
+        // Iterate each entry and only getting child info
+        while(it.hasNext())
+        {
+            Map.Entry<String,String> pair = (Map.Entry) it.next();
+            
+            // Child mapping detected
+            if(pair.getKey().contains("~"))
+            {
+                String key = pair.getKey(); // Reconcilition Multivauled Name and Field E.g. BadgeUD_ACCESS Child~Name
+                String value = pair.getValue(); // Target table and column E.g. MY_BADGE_ENT_RE_FEED~NAME
+
+                String[] reconMapAndField = key.split("~");
+                String[] targetTableAndColumn = value.split("~");
+
+                String rfMultiValueName = reconMapAndField[0];
+                String targetChildTable = targetTableAndColumn[0];
+
+                String childFieldName = reconMapAndField[1];
+                String childColumnName = targetTableAndColumn[1];
+
+                // Multivalued Field Map exist already; adding additional column
+                if(childTableMappings.containsKey(rfMultiValueName))
+                {
+                    childColumnMappings.get(rfMultiValueName).put(childFieldName, childColumnName);  
+                }
+
+                // First time inspecting child table
+                else
+                {
+                    childTableMappings.put(rfMultiValueName, targetChildTable); // Put Multivalued Field and target table name
+
+                    HashMap<String,String> fieldToColumnMap = new HashMap<String,String>();
+                    fieldToColumnMap.put(childFieldName, childColumnName);
+
+                    childColumnMappings.put(rfMultiValueName, fieldToColumnMap); // Put Multivalued Map Name and field to column correspondence
+                }
+                
+                // Remove child info from Recon Attr Map
+                reconAttrMap.remove(key);
+            }
+        }
+    }
+    
+    /**
      * Get database connection from a data source
      * using the JNDI Name
      * @param jndiName  JNDI Name
@@ -181,10 +246,13 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
      * @param actionDate For deferring events
      * @param reconAttrMap Reconciliation Attribute Mappings
      * @param itResName IT Resource Name; Used for target resources; Empty for trusted
+     * @param childTableMappings Map of Reconciliation Field Map Name to Target Table Name 
+     * @param childColumnMappings Map of Reconciliation Field Map Name and corresponding field name to target column name
+     * @param linkColumnName Column to link parent to child table
      * @return List of events to be created
      * @throws SQLException 
      */
-    public List<InputData> constructReconciliationEventList(Connection conn, String tableName, String filter, Boolean eventFinished, Date actionDate, HashMap<String,String> reconAttrMap, String itResName) throws SQLException
+    public List<InputData> constructReconciliationEventList(Connection conn, String tableName, String filter, Boolean eventFinished, Date actionDate, HashMap<String,String> reconAttrMap, String itResName, HashMap<String,String> childTableMappings, HashMap<String,HashMap<String,String>> childColumnMappings, String linkColumnName) throws SQLException
     {
         List<InputData> allReconEvents = new ArrayList<InputData>();
         
@@ -210,12 +278,12 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
             // Use Lookup to translate mappings
             if(useTranslateMap)
             {
-                // Iterate Attr Mappings
+                // Iterate Attr Mappings Lookup
                 for(Map.Entry<String,String> entry : reconAttrMap.entrySet())
                 {
-                    String reconFieldName = entry.getKey();
-                    String targetColumnName = entry.getValue();
-                   
+                    String reconFieldName = entry.getKey(); // Code Key
+                    String targetColumnName = entry.getValue(); // Decode
+                                       
                     // IT Resource Name Field; Only for target
                     if("__SERVER__".equals(targetColumnName))
                     {
@@ -232,6 +300,7 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
             }
             
             // Target columns are identical to reconciliation field names
+            // No Child data supported for this optional
             else
             {
                 // Iterate each column and populate map accordingly
@@ -251,6 +320,48 @@ public class ReconEventsGeneratorDatabaseSource extends TaskSupport
         }
         
         return allReconEvents;
+    }
+    
+    private HashMap<String,ArrayList<HashMap<String,Object>>> fetchUserEntitlements(Connection conn, HashMap<String,String> childTableMappings, HashMap<String,HashMap<String,String>> childColumnMappings, String linkColumnName, String linkColumnValue) throws SQLException
+    { 
+        HashMap<String,ArrayList<HashMap<String,Object>>> childReconData = new HashMap<String,ArrayList<HashMap<String,Object>>>(); // {Key = Child Recon Field Map Name, Value = {Key = Child Recon Field Name, Value = data}} 
+         
+        // Iterate each child table
+        for(Map.Entry<String,String> entry : childTableMappings.entrySet())
+        {
+            String rfMapName = entry.getKey();
+            String childTableName = entry.getValue();
+            
+            // Fetch user entitlement records from child table
+            String userEntQuery = "SELECT * FROM " + childTableName + " WHERE " + linkColumnName + " = ?";
+            PreparedStatement ps = conn.prepareStatement(userEntQuery);
+            ps.setString(1, linkColumnValue);
+            
+            ResultSet rs = ps.executeQuery();
+            ArrayList<HashMap<String,Object>> childEntries = new ArrayList<HashMap<String,Object>>();
+            
+            // iterate result set
+            while(rs.next())
+            {
+                HashMap<String,String> columnMap = childColumnMappings.get(rfMapName);
+                HashMap<String,Object> childRecordData = new HashMap<String, Object>();
+                
+                for(Map.Entry<String,String> cEntry : columnMap.entrySet())
+                {
+                    String rfName = cEntry.getKey();
+                    String columnName = cEntry.getValue();
+                    
+                    String columnValue = rs.getString(columnName);
+                    childRecordData.put(rfName, columnValue); // Populate child recon field with corresponding target column
+                }
+                
+                childEntries.add(childRecordData);   
+            }
+            
+            childReconData.put(rfMapName, childEntries);
+        }
+        
+        return childReconData;
     }
     
     /**
